65ddaa72f3d1e6bfc85c28319c9b3a6a3480f694
Santi Teran <santiagoteranmatias@gmail.com>
2024-05-09 15:20:30 -0300

    refactor(-): navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Image from 'next/image';
+import starwars from '../../public/starwars.png'
 
 const NavBar = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col items-center'>
+      <Image src={starwars} alt='StarWars'/>
+      <h2 className='text-3xl font-thin text-white mb-32'>DISCOVERY</h2>
     </div>
   )
 }
