bea386ffe81c8bf36a1f7cbb37918df18df474ad
Santi Teran <santiagoteranmatias@gmail.com>
2024-05-09 15:18:59 -0300

    refactor(-): homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
 import Background from "@/components/Background";
+import Image from "next/image";
+import films from "../../public/films.png"
+import characters from "../../public/characters.png"
+import Link from "next/link";
 
 const Home = () => {
   return (
-    <div>
-      Hello World
+    <div className="flex justify-center gap-x-40">
       <Background />
+      <Link href='/films' className="flex flex-col items-center hover:scale-125 transition-all">
+        <Image src={films} alt="Characters" className=""/>
+        <h2 className='text-3xl font-bold text-white'>FILMS</h2>
+      </Link>
+      <Link href='/characters' className="flex flex-col items-center hover:scale-125 transition-all">
+        <Image src={characters} alt="Characters" className=""/>
+        <h2 className='text-3xl font-bold text-white'>CHARACTERS</h2>
+      </Link>
     </div>
   )
 }
