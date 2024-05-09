f120e885c035c15fbc287290b65f4f297dadc857
Santi Teran <santiagoteranmatias@gmail.com>
2024-05-09 17:01:29 -0300

    refactor(-): Films

--- src/app/films/page.jsx
+++ src/app/films/page.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import FilmsComponent from '@/components/FilmsComponent';
+const getFilms = async () => {
+  const res = await fetch('https://swapi.dev/api/films/');
+  
+  if (!res.ok) throw new Error('Something went wrong');
 
-const Films = () => {
+  return res.json();
+};
+
+const Films = async () => {
+
+  const films = await getFilms();
+
   return (
-    <div>
-      
+    <div className=' grid grid-cols-3 gap-20 p-32'>
+      { films.results.map((film) => (
+        <div key={film.episode_id}>
+          <FilmsComponent film={film} />
+        </div>
+      ))}
     </div>
   )
 }
