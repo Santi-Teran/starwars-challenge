0aa547bca7db5c4f54065502af74ded54a25bbab
Santi Teran <santiagoteranmatias@gmail.com>
2024-05-10 21:07:07 -0300

    add(-): create pagination

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,27 @@
+import { MdNavigateBefore, MdNavigateNext } from "react-icons/md";
+
+
+const Pagination = ({ page, totalPages, goToPage, nextPage, prevPage }) => {
+  const pageNumbers = [];
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  };
+
+  return (
+    <div className='flex my-8 py-2 px-4 bg-gray-400 w-fit mx-auto rounded-full shadow-lg'>
+      <button onClick={prevPage} disabled={page === 1} className="text-4xl hover:scale-125 transition-all cursor-pointer"><MdNavigateBefore /></button>
+      {pageNumbers.map((pageNumber) => (
+        <button
+          key={pageNumber}
+          onClick={() => goToPage(pageNumber)}
+          className={`mx-5 py-2 px-4 rounded-full hover:scale-125 transition-all ${pageNumber === page ? 'bg-blue-500 hover:scale-100 transition-all' : ''}`}
+        >
+          {pageNumber}
+        </button>
+      ))}
+      <button onClick={nextPage} disabled={page === totalPages} className="text-4xl hover:scale-125 transition-all cursor-pointer"><MdNavigateNext /></button>
+    </div>
+  );
+};
+
+export default Pagination;
