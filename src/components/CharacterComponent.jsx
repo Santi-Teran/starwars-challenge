9b2d62f11c39c987d3272d6b5e9810adea088751
Santi Teran <santiagoteranmatias@gmail.com>
2024-05-10 21:06:46 -0300

    add(-): create charactercomponent

---
+++ src/components/CharacterComponent.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import darth from '../../public/darth.jpeg'
+import Link from 'next/link';
+
+const CharacterComponent = ({ character }) => {
+
+  const id = character.url.split('/')[5];
+
+  return (
+    <div className='flex flex-col gradient rounded-xl shadow-xl w-3/5 mx-auto hover:scale-110 transition-all'>
+      <Link href={`/characters/${id}`} >
+        <Image src={darth} alt='Darth Vader' className='rounded-t-xl'/>
+        <div className='p-2 text-white'>
+          <h2 className='text-xl font-bold'>{character.name}</h2>
+          <p className='text-sm font-extralight'>{character.gender !== 'n/a' ? character.gender.toUpperCase() : '-'}</p>
+          <p className='text-sm font-extralight'>{character.eye_color !== 'unknown' ? character.eye_color.toUpperCase() : '-'}</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default CharacterComponent;
