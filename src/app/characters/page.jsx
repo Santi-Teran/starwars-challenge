ce8b39ff0292c0dd27a52975132ef9864884badf
Santi Teran <santiagoteranmatias@gmail.com>
2024-05-13 18:14:33 -0300

    refactor(-): fix components

--- src/app/characters/page.jsx
+++ src/app/characters/page.jsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import CharacterComponent from '@/components/CharacterComponent';
 import Pagination from '@/components/Pagination'; 
 import NavBar from '@/components/NavBar';
+import Loader from '@/components/Loader';
+import Footer from '@/components/Footer';
 
 const getCharacters = async (page) => {
   const res = await fetch(`https://swapi.dev/api/people/?page=${page}`);
@@ -17,43 +19,64 @@ const Characters = () => {
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(false);
-
-  const fetchCharacters = async (pageNumber) => {
-    setLoading(true);
-    try {
-      const data = await getCharacters(pageNumber);
-      setCharacters(data.results);
-      setTotalPages(Math.ceil(data.count / 10));
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const [searchTerm, setSearchTerm] = useState('');
+  const [genderFilter, setGenderFilter] = useState('');
+  const [eyeColorFilter, setEyeColorFilter] = useState('');
 
   useEffect(() => {
+    const fetchCharacters = async (pageNumber) => {
+      setLoading(true);
+      try {
+        const data = await getCharacters(pageNumber);
+        let filteredCharacters = data.results.filter(character =>
+          character.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        if (genderFilter) {
+          filteredCharacters = filteredCharacters.filter(character =>
+            character.gender.toLowerCase() === genderFilter.toLowerCase()
+          );
+        }
+        if (eyeColorFilter) {
+          filteredCharacters = filteredCharacters.filter(character =>
+            character.eye_color.toLowerCase() === eyeColorFilter.toLowerCase()
+          );
+        }
+        setCharacters(filteredCharacters);
+        setTotalPages(Math.ceil(data.count / 10));
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+  
     fetchCharacters(page);
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }, [page]);
+  }, [page, searchTerm, genderFilter, eyeColorFilter]);  
 
   const nextPage = () => setPage(page + 1);
   const prevPage = () => setPage(page - 1);
   const goToPage = (pageNumber) => setPage(pageNumber);
 
+  const handleSearchInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleGenderFilterChange = (e) => {
+    setGenderFilter(e.target.value);
+  };
+
+  const handleEyeColorFilterChange = (e) => {
+    setEyeColorFilter(e.target.value);
+  };
+
   return (
-    <div className='star-wars-gradient bg-neutral-400 text-white'>
-      <NavBar />
-      {/* {Array.from({ length: 500 }).map((_, index) => (
-        <div
-          key={index}
-          className='star'
-          style={{
-            '--star-top': Math.random(),
-            '--star-left': Math.random(),
-            '--animation-delay': Math.random() * 10
-          }}
-        />
-      ))} */}
+    <div className='star-wars-gradient text-white'>
+      <NavBar 
+        handleSearchInputChange={handleSearchInputChange}
+        handleGenderFilterChange={handleGenderFilterChange}
+        handleEyeColorFilterChange={handleEyeColorFilterChange}
+      />
+
       <Pagination
         page={page}
         totalPages={totalPages}
@@ -63,9 +86,11 @@ const Characters = () => {
       />
 
       {loading ? (
-        <div className="text-center">Cargando...</div>
+        <Loader />
+      ) : ( characters.length == 0 ) ? (
+        <div className='flex justify-center my-40 text-4xl'>No characters with those filters</div> 
       ) : (
-        <div className='grid grid-cols-3 gap-10 mx-20 z-10'>
+        <div className='grid grid-cols-3 gap-10 my-10 mx-20 z-10'>
           {characters.map((character) => (
             <div key={character.url}>
               <CharacterComponent character={character} />
@@ -81,6 +106,8 @@ const Characters = () => {
         nextPage={nextPage}
         prevPage={prevPage}
       />
+
+      <Footer />
     </div>
   );
 };
